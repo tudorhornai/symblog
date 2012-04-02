9185a90bc33a2f636205edec026f6463f9115deb
Tudor Hornai <tudz_h@yahoo.com>
2012-04-03 01:20:16 +0300

    - finished part 1

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -11,4 +11,9 @@ class PageController extends Controller
     {
         return $this->render('BloggerBlogBundle:Page:index.html.twig');
     }
+	
+	public function aboutAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
 }
